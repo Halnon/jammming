eb94cfee228749bf89ebd065a192f0799827988d
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 14:59:42 +0100

    refactor SearchBar class into funnction

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-export class SearchBar extends React.Component {
-    render() {
-        return (
-            <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist"/>
-                <button className="SearchButton">SEARCH</button>
-            </div>
-        )
-    }
+export default function SearchBar() {
+    return (
+        <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist"/>
+            <button className="SearchButton">SEARCH</button>
+        </div>
+    )
 }
