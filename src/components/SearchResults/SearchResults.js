fb28097053120ae862e6e122b9b2ca88781a7bcc
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 15:19:04 +0100

    initial commit

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Styles from './SearchResults.module.css'
+
+export default function SearchResults () {
+    return (
+        <div className={Styles.SearchResults}>
+        {/* <!-- Add a TrackList component --> */}
+      </div>
+    );
+}
