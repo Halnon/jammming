f89fbb635719a220622adbe16d8b2920b9cad908
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 16:31:50 +0100

    import styles and refect classname value

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -4,7 +4,7 @@ import Styles from './Track.module.css'
 export default function Track () {
     return (
       <div className={Styles.Track}>
-        <div className="Track-information">
+        <div className={Styles.Track-information}>
           {/* <h3><!-- track name will go here --></h3> */}
           
           {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
