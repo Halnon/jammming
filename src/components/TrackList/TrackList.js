85d50ac885c847a98dbfb7b947285fefab637cf6
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 15:51:08 +0100

    import styles

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Styles from './TrackList.module.css'
 
 export default function Tracklist () {
     return (
-        <div className="TrackList">
+        <div className={Styles.TrackList}>
         {/* <!-- You will add a map method that renders a set of Track components  --> */}
       </div>
     );
