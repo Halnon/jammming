5bb34ce2540f9fe07a292c72f52f52c4bec41d73
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 16:34:25 +0100

    minor amendment to track info classname

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -4,7 +4,7 @@ import Styles from './Track.module.css'
 export default function Track () {
     return (
       <div className={Styles.Track}>
-        <div className={Styles.Track-information}>
+        <div className={Styles.Track_information}>
           {/* <h3><!-- track name will go here --></h3> */}
           
           {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
