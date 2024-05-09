12e4b199aae8560b0544e4a88c9d3e66f711edf9
Adam Halnon <halnon_01@hotmail.com>
2024-05-09 14:57:36 +0100

    refactor app class into funnction

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,24 +2,20 @@ import React from 'react';
 import Styles from './App.module.css';
 import { SearchBar } from '../SearchBar/SearchBar.js';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>
-          Ja<span className={Styles.highlight}>mmm</span>ing
-        </h1>
-      <div className={Styles.App}>
-        <SearchBar />
-        
-        <div className={Styles.App_playlist}>
-          {/* <!-- Add a SearchResults component --> */}
-          {/* <!-- Add a Playlist component --> */}
-        </div>
+export default function App () {
+  return (
+    <div>
+      <h1>
+        Ja<span className={Styles.highlight}>mmm</span>ing
+      </h1>
+    <div className={Styles.App}>
+      <SearchBar />
+      
+      <div className={Styles.App_playlist}>
+        {/* <!-- Add a SearchResults component --> */}
+        {/* <!-- Add a Playlist component --> */}
       </div>
     </div>
-    );
-  }
+  </div>
+  );
 }
-
-export default App;
