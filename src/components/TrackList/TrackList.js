2b13538bd88fac8d440b2f238782c9101f3808eb
Adam Halnon <halnon_01@hotmail.com>
2024-06-18 18:25:38 +0100

    map through searchResults array using map and render Track component for each

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -2,14 +2,12 @@ import React from "react";
 import Styles from './TrackList.module.css'
 import Track from '../Track/Track'
 
-export default function Tracklist () {
+export default function Tracklist (props) {
     return (
         <div className={Styles.Tracklist}>
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
-        Track 1
-        Track 2
-        Track 3
-        Track 4
+          {props.userSearchResults.map(track => {
+            return <Track track={track} key={track.id} />
+          })}
       </div>
     );
 }
