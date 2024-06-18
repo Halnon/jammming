77a8c255a089a4170ab9ab055852361caf4083ff
Adam Halnon <halnon_01@hotmail.com>
2024-06-18 18:02:04 +0100

    import usestate and set initial hard coded values for name, artist, and album for search results

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Styles from './App.module.css';
 import SearchBar from '../SearchBar/SearchBar.js';
 import SearchResults from '../SearchResults/SearchResults.js';
 import Playlist from '../Playlist/Playlists.js';
 
 export default function App () {
+
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: 'example track name 1', 
+      artist: 'example artist name 1', 
+      album: 'example album name 1',
+      id: 1
+    },
+    {
+      name: 'example track name 2', 
+      artist: 'example artist name 2', 
+      album: 'example album name 2',
+      id: 1
+    }]
+  );
+
   return (
     <div>
       <h1>
