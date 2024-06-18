a495f8a9e6bed470847c4cad655b1a706c213274
Adam Halnon <halnon_01@hotmail.com>
2024-06-18 17:33:54 +0100

    add renderAction function to Track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Styles from './Track.module.css'
 
-export default function Track () {
+export default function Track (props) {
+  function renderAction() {
+    if (props.isRemoval) {
+      return <button className={Styles.Track_action}>-</button>
+    } else {
+      return <button className={Styles.Track_action}>-</button>
+    }
+  }
+
     return (
       <div className={Styles.Track}>
         <div className={Styles.Track_information}>
