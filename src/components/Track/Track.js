659c62e75dc78e8d01a7dc4ba126115d0f198ca7
Adam Halnon <halnon_01@hotmail.com>
2024-06-18 18:36:52 +0100

    render track info via props passed from tracklist components map function

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -13,9 +13,8 @@ export default function Track (props) {
   return (
     <div className={Styles.Track}>
       <div className={Styles.Track_information}>
-        {/* <h3><!-- track name will go here --></h3> */}
-        
-        {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+        {<h3>{props.track.name}</h3>}
+        {<p>{props.track.artist} | {props.track.album}</p>}
       </div>
       {/* <button class="Track-action"><!-- + or - will go here --></button> */}
     </div>
