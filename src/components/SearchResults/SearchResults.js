e0c9e02bf4961143aa489598831a76c6c868b17b
Adam Halnon <halnon_01@hotmail.com>
2024-06-18 18:12:37 +0100

    pass usersearchresults props from SearchResults component to nested tracklist component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,10 +2,10 @@ import React from "react";
 import Styles from './SearchResults.module.css';
 import TrackList from '../TrackList/TrackList.js'
 
-export default function SearchResults () {
+export default function SearchResults (props) {
     return (
         <div className={Styles.SearchResults}>
-        <TrackList />
+        <TrackList userSearchResults={props.userSearchResults} />
       </div>
     );
 }
