7cb8151abfba92f31ec153d76e3b0683ff31cce1
Adam Halnon <halnon_01@hotmail.com>
2024-06-27 09:47:59 +0100

    format code better

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -5,9 +5,14 @@ import Track from '../Track/Track'
 export default function Tracklist (props) {
     return (
         <div className={Styles.Tracklist}>
-          {props.userSearchResults.map(track => {
-            return <Track track={track} key={track.id} isRemoval={props.isRemoval} onAdd={props.onAdd} />
-          })}
+          {props.userSearchResults.map((track) => (
+            <Track 
+              track={track} 
+              key={track.id} 
+              isRemoval={props.isRemoval} 
+              onAdd={props.onAdd} 
+              />
+          ))}
       </div>
     );
 }
