bf944597f872d91395410513b794a9f31dd9d590
Adam Halnon <halnon_01@hotmail.com>
2024-06-28 09:50:56 +0100

    pass onremove func to tracklist and set isremoval attri

--- src/components/Playlist/Playlists.js
+++ src/components/Playlist/Playlists.js
@@ -6,7 +6,7 @@ export default function Playlist(props) {
   return (
     <div className={Styles.Playlist}>
       <input defaultValue={"New Playlist"} />
-      <Tracklist userSearchResults={props.playlistTracks} />
+      <Tracklist userSearchResults={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
       <button className={Styles.Playlist_save}>
         SAVE TO SPOTIFY
       </button>
