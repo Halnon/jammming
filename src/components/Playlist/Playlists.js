d67d23021622e90936485b3c28266f5bef43eb95
Adam Halnon <halnon_01@hotmail.com>
2024-07-02 12:23:22 +0100

    create handlenamechange func that takes event triggered by onchange attr passed fromm app

--- src/components/Playlist/Playlists.js
+++ src/components/Playlist/Playlists.js
@@ -3,10 +3,22 @@ import Styles from './Playlist.module.css'
 import Tracklist from "../TrackList/TrackList";
 
 export default function Playlist(props) {
+
+  function handleNameChange({ target }) {
+    props.onNameChange(target.value);
+  };
+
   return (
     <div className={Styles.Playlist}>
-      <input defaultValue={"New Playlist"} />
-      <Tracklist userSearchResults={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
+      <input 
+        defaultValue={"New Playlist"} 
+        onChange={handleNameChange} 
+      />
+      <Tracklist 
+        userSearchResults={props.playlistTracks} 
+        onRemove={props.onRemove} 
+        isRemoval={true} 
+      />
       <button className={Styles.Playlist_save}>
         SAVE TO SPOTIFY
       </button>
