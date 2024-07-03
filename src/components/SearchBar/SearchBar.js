e8d92faf32aaeb1920d6435eb5e096c373803938
Adam Halnon <halnon_01@hotmail.com>
2024-07-03 11:24:52 +0100

    import usestate and initialise term state

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Styles from './SearchBar.module.css';
 
 export default function SearchBar() {
+    const [term, setTerm] = useState("");
+
     return (
         <div className={Styles.SearchBar}>
             <input placeholder="Enter A Song, Album, or Artist"/>
