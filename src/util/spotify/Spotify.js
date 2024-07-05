054238af84b25dcc9de434afbc33524192fe28bb
Adam Halnon <halnon_01@hotmail.com>
2024-07-05 13:06:24 +0100

    create getaccesstoken method in empty object

--- src/util/spotify/Spotify.js
+++ src/util/spotify/Spotify.js
@@ -1,5 +1,26 @@
-let userAccessToken;
+let accessToken;
 
-const Spotify = {};
+const Spotify = {
+    getAccessToken() {
+        if (accessToken)
+            return accessToken;
+            // extract access token from URL
+            const tokenInURL = window.location.href.match(/access_token=([^&]*)/);
+            // extract expiry time from URL
+            const expiryTimeFromURL = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (tokenInURL && expiryTimeFromURL) {
+            accessToken = tokenInURL[1];
+            const expiresIn = Number(expiryTimeFromURL[1]);
+
+            //set function which will reset access token when it expires
+            window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+
+            // Clear the parameters from the URL, so the app doesn't try grabbing the access token after it has expired
+            window.history.pushState("Access token", null, "/");
+            return accessToken;
+        }
+    }
+};
 
 export {Spotify};
