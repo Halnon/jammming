a037431aa670cc253440f2638a7c39d0ff993cba
Adam Halnon <halnon_01@hotmail.com>
2024-12-28 17:14:10 +0000

    comment out default data in usestate

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,40 +8,42 @@ import {Spotify} from '../../util/spotify/Spotify.js';
 export default function App () {
 
   const [searchResults, setSearchResults] = useState([
-    {
-      name: 'example track name 1', 
-      artist: 'example artist name 1', 
-      album: 'example album name 1',
-      id: 1
-    },
-    {
-      name: 'example track name 2', 
-      artist: 'example artist name 2', 
-      album: 'example album name 2',
-      id: 2
-    },
-    {
-      name: 'example track name 3', 
-      artist: 'example artist name 3', 
-      album: 'example album name 3',
-      id: 3
-    }]
+      // {
+      //   name: 'example track name 1', 
+      //   artist: 'example artist name 1', 
+      //   album: 'example album name 1',
+      //   id: 1
+      // },
+      // {
+      //   name: 'example track name 2', 
+      //   artist: 'example artist name 2', 
+      //   album: 'example album name 2',
+      //   id: 2
+      // },
+      // {
+      //   name: 'example track name 3', 
+      //   artist: 'example artist name 3', 
+      //   album: 'example album name 3',
+      //   id: 3
+      // }
+    ]
   );
 
   const [playlistName, setPlaylistName] = useState('Example playlist name');
   const [playlistTracks, setPlaylistTracks] = useState([
-    {
-      name: 'example playlist name 1', 
-      artist: 'example playlist artist 1', 
-      album: 'example playlist album 1',
-      id: 1
-    },
-    {
-      name: 'example playlist name 2', 
-      artist: 'example playlist artist 2', 
-      album: 'example playlist album 2',
-      id: 2
-    }]
+      // {
+      //   name: 'example playlist name 1', 
+      //   artist: 'example playlist artist 1', 
+      //   album: 'example playlist album 1',
+      //   id: 1
+      // },
+      // {
+      //   name: 'example playlist name 2', 
+      //   artist: 'example playlist artist 2', 
+      //   album: 'example playlist album 2',
+      //   id: 2
+      // }
+    ]
   ); 
 
   function addTrack(track) {
