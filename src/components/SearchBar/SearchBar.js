6bc7561e13366c4f2b75d7ba7c7c43b04b983c28
Adam Halnon <halnon_01@hotmail.com>
2024-12-28 17:15:12 +0000

    amend script to prevent app reload on button click

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,10 @@ import Styles from './SearchBar.module.css';
 export default function SearchBar(props) {
     const [term, setTerm] = useState("");
 
-    function passTerm() {
+    function passTerm(event) {
+        if (event) {
+            event.preventDefault();
+        }
         props.onSearch(term);
     };
 
@@ -17,10 +20,12 @@ export default function SearchBar(props) {
             <input 
                 placeholder="Enter A Song, Album, or Artist"
                 onChange={handleTermChange}
+                value={term}
             />
             <button 
                 className={Styles.SearchButton}
                 onClick={passTerm}
+                type="button"
             >SEARCH</button>
         </div>
     )
